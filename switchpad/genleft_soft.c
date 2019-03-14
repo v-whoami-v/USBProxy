0aed9d269ac22851a0ab18acd073c86606562df1
v-whoami-v <46952670+v-whoami-v@users.noreply.github.com>
2019-03-14 21:33:11 +0900

    Add files via upload

---
+++ switchpad/genleft_soft.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void output(const char *str) {
+    FILE *fp = fopen("/run/input.txt", "w");
+    if (fp == NULL) {
+        fprintf(stderr, "cannot open: %s\n", "/run/input.txt");
+        return;
+    }
+
+    fprintf(fp, "%s", str);
+    fclose(fp);
+}
+
+int upper[] = {2, 3, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38};
+int lower[] = {253, 252, 251, 251, 250, 249, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 237, 236, 235, 234, 233, 232, 231, 230, 229, 228, 227, 226, 225, 224, 223, 222, 221, 220, 219, 218, 217};
+
+int upper2[] = { 66, 66, 66, 67, 67, 68, 68, 69, 69, 70, 70, 71, 71};
+int lower2[] = {190,190,189,189,188,188,187,187,186,186,185,185,184};
+
+int main() {
+    int i;
+    char buf[30];
+
+    for (;;) {
+        for (i=0; i<37; i++) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", i, upper[i]); output(buf); usleep(1000 * 5);
+        }
+        for (i=upper[37]; i<lower[37]; i++) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", 37, i);
+            output(buf);
+            usleep(1000 * 5);
+        }
+        for (i=37; 0<i; i--) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", i, lower[i]);
+            output(buf);
+            usleep(1000 * 5);
+        }
+        for (i=lower[0]; lower2[0]<i; i--) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", 0, i);
+            output(buf);
+            usleep(1000 * 5);
+        }
+        for (i=0; i<13; i++) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", i, lower2[i]);
+            output(buf);
+            usleep(1000 * 5);
+        }
+        for (i=lower2[12]-1; upper2[12]<i; i--) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", 13, i);
+            output(buf);
+            usleep(1000 * 5);
+        }
+        for (i=12; 0<i; i--) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", i, upper2[i]);
+            output(buf);
+            usleep(1000 * 5);
+        }
+        for (i=upper2[0]; upper[0]<i; i--) {
+            sprintf(buf, "00 00 0f %02x %02x 80 80 00", 0, i);
+            output(buf);
+            usleep(1000 * 5);
+        }
+    }
+}
+
