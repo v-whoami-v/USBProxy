512c0b0b86f96adec1e7ac52d2b947f37122301a
v-whoami-v <46952670+v-whoami-v@users.noreply.github.com>
2021-03-11 16:44:22 +0900

    Update PacketFilter_SwitchPad.h

--- src/Filters/PacketFilter_SwitchPad.h
+++ src/Filters/PacketFilter_SwitchPad.h
@@ -8,7 +8,7 @@ private:
 	void convData(__u8 *data, __u16 len);
 public:
 	PacketFilter_SwitchPad();
-	void filter_packet(Packet* packet);
+	virtual void filter_packet(Packet* packet);
 	virtual char* toString() {return (char*)"Switch GamePad Filter";}
 };
 #endif /* PACKETFILTER_SWITCHPAD_H_ */
